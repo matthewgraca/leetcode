f30a241abaff2325a3b95ed1c3279918412fa61b
matthewgraca <mgraca@cpp.edu>
2021-07-12 21:41:23 -0700

    added base for prob 10

--- src/main/java/mgraca/hard/RegularExpressionMatching.java
+++ src/main/java/mgraca/hard/RegularExpressionMatching.java
@@ -1,5 +1,24 @@
 package mgraca.hard;
 
+/*
+ * Description: Given an input string (s) and a pattern (p), implement regular expression matching 
+ *  with support for '.' and '*' where: 
+ *  '.' Matches any single character. 
+ *  '*' Matches zero or more of the preceding element.
+ *  The matching should cover the entire input string (not partial).
+ *
+ * Constraints:
+ *  0 <= s.length <= 20
+ *  0 <= p.length <= 30
+ *  s contains only lowercase English letters.
+ *  p contains only lowercase English letters, '.', and '*'.
+ *  It is guaranteed for each appearance of the character '*', there will be a previous valid 
+ *    character to match.
+ * 
+ * Complexity:
+ *  Time:
+ *  Space:
+ */
 public class RegularExpressionMatching{
   public static boolean isMatch(String p, String s){
     return false;
--- src/test/java/mgraca/hard/RegularExpressionMatchingTest.java
+++ src/test/java/mgraca/hard/RegularExpressionMatchingTest.java
@@ -5,8 +5,28 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class RegularExpressionMatchingTest{
-  @Test
+  //@Test
   public void example1(){
-    assertTrue(RegularExpressionMatching.isMatch("", "") == false);
+    assertTrue(RegularExpressionMatching.isMatch("aa", "a") == false);
+  }
+
+  //@Test
+  public void example2(){
+    assertTrue(RegularExpressionMatching.isMatch("aa", "a*") == true);
+  }
+
+  //@Test
+  public void example3(){
+    assertTrue(RegularExpressionMatching.isMatch("ab", ".*") == true);
+  }
+
+  //@Test
+  public void example4(){
+    assertTrue(RegularExpressionMatching.isMatch("aab", "c*a*b") == true);
+  }
+
+  //@Test
+  public void example5(){
+    assertTrue(RegularExpressionMatching.isMatch("mississippi", "mis*is*p*") == false);
   }
 }
