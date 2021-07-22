c8c80e36c9dd9d7184137dd17b785d5f358e4e0e
matthewgraca <mgraca@cpp.edu>
2021-07-21 21:23:02 -0700

    added version using binary search

--- src/main/java/mgraca/medium/ThreeSum.java
+++ src/main/java/mgraca/medium/ThreeSum.java
@@ -20,12 +20,17 @@ import java.util.Arrays;
  */
 public class ThreeSum{
   /*
-   * Time:  O(n^3)
-   * Space: O(n)
+   * Time:  O(n^9)
+   *  +nlogn for sorting
+   *  +n choose 3 -> compare all combinations of 3 from n items, each with 
+   *    3 * (n^3 choose 2) -> worst case, all solution triples are valid and checked 
+   *    worst case: add 1 check 1, add 1 check 2, add 1 check 3, ..., add 1 check n
+   * Space: O(n^3)
+   *  +n choose 3 -> worst case, every combination of triples is valid and stored
    */
   public static List<List<Integer>> naiveThreeSum(int[] nums){
     List<List<Integer>> solution = new ArrayList<>();
-    Arrays.sort(nums);
+    Arrays.sort(nums);  // ensures the triples are sorted
     for (int i = 0; i < nums.length; i++){
       for (int j = i + 1; j < nums.length; j++){
         for (int k = j + 1; k < nums.length; k++){
@@ -42,12 +47,31 @@ public class ThreeSum{
   }
 
   /*
-   * Time:  
-   * Space:
+   * Time:  O(n^8logn) 
+   *  +nlogn for sorting
+   *  +n^2logn for comparing each pair against a sorted value in the array using binary search 
+   *  +3 * n^3 choose 2 = n^6, worst case every triple is valid and checked against
+   * Space: O(n^3)
+   *  +worst case, every combination of triples is valid and stored
    */
-  public static List<List<Integer>> fastThreeSum(int[] nums){
+  public static List<List<Integer>> sortThreeSum(int[] nums){
     List<List<Integer>> solution = new ArrayList<>();
     Arrays.sort(nums);
+    for (int i = 0; i < nums.length; i++){
+      for (int j = i + 1; j < nums.length; j++){
+        int k = Arrays.binarySearch(nums, j+1, nums.length, -(nums[i]+nums[j]));
+        if (k >= 0){
+          List<Integer> solutionPiece = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k]));
+          if (!solution.contains(solutionPiece)){
+            solution.add(solutionPiece);
+          }
+        }
+      }
+    }
+    return solution;
+  }
+
+  public static List<List<Integer>> hashThreeSum(int[] nums){
     return null;
   }
 }
--- src/test/java/mgraca/medium/ThreeSumTest.java
+++ src/test/java/mgraca/medium/ThreeSumTest.java
@@ -43,36 +43,68 @@ public class ThreeSumTest{
   }
 
   @Test
-  public void fastExample1(){
+  public void sortExample1(){
     int[] input = new int[]{-1, 0, 1, 2, -1, -4};
     List<List<Integer>> expectedOutput = new ArrayList<>();
     expectedOutput.add(new ArrayList<>(Arrays.asList(-1, -1, 2)));
     expectedOutput.add(new ArrayList<>(Arrays.asList(-1, 0, 1)));
 
-    List<List<Integer>> actualOutput = ThreeSum.fastThreeSum(input);
+    List<List<Integer>> actualOutput = ThreeSum.sortThreeSum(input);
     String errorMsg = "Expected " + expectedOutput.toString() + 
                       ", returned " + actualOutput.toString();
     assertTrue(errorMsg, actualOutput.equals(expectedOutput));
   }
 
   @Test
-  public void fastExample2(){
+  public void sortExample2(){
     int[] input = new int[0];
     List<List<Integer>> expectedOutput = new ArrayList<>();
-    List<List<Integer>> actualOutput = ThreeSum.fastThreeSum(input);
+    List<List<Integer>> actualOutput = ThreeSum.sortThreeSum(input);
     String errorMsg = "Expected " + expectedOutput.toString() + 
                       ", returned " + actualOutput.toString();
     assertTrue(errorMsg, actualOutput.equals(expectedOutput));
   }
 
   @Test
-  public void fastExample3(){
+  public void sortExample3(){
     int[] input = new int[]{0};
     List<List<Integer>> expectedOutput = new ArrayList<>();
-    List<List<Integer>> actualOutput = ThreeSum.fastThreeSum(input);
+    List<List<Integer>> actualOutput = ThreeSum.sortThreeSum(input);
     String errorMsg = "Expected " + expectedOutput.toString() + 
                       ", returned " + actualOutput.toString();
     assertTrue(errorMsg, actualOutput.equals(expectedOutput));
   }
 
+  @Test
+  public void hashExample1(){
+    int[] input = new int[]{-1, 0, 1, 2, -1, -4};
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    expectedOutput.add(new ArrayList<>(Arrays.asList(-1, -1, 2)));
+    expectedOutput.add(new ArrayList<>(Arrays.asList(-1, 0, 1)));
+
+    List<List<Integer>> actualOutput = ThreeSum.hashThreeSum(input);
+    String errorMsg = "Expected " + expectedOutput.toString() + 
+                      ", returned " + actualOutput.toString();
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  @Test
+  public void hashExample2(){
+    int[] input = new int[0];
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    List<List<Integer>> actualOutput = ThreeSum.hashThreeSum(input);
+    String errorMsg = "Expected " + expectedOutput.toString() + 
+                      ", returned " + actualOutput.toString();
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  @Test
+  public void hashExample3(){
+    int[] input = new int[]{0};
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    List<List<Integer>> actualOutput = ThreeSum.hashThreeSum(input);
+    String errorMsg = "Expected " + expectedOutput.toString() + 
+                      ", returned " + actualOutput.toString();
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
 }
