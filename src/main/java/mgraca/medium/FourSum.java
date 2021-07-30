0d46b3af076e6e3a82fd002eb2ec5b9c49c58f8e
matthewgraca <mgraca@cpp.edu>
2021-07-30 15:35:39 -0700

    added solution and extra tests

--- src/main/java/mgraca/medium/FourSum.java
+++ src/main/java/mgraca/medium/FourSum.java
@@ -2,9 +2,63 @@ package mgraca.medium;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
 
+/*
+ * Description: Given an array nums of n integers, return an array of all the unique quadruplets 
+ *  [nums[a], nums[b], nums[c], nums[d]] such that:
+ *  0 <= a, b, c, d < n
+ *  a, b, c, and d are distinct.
+ *  nums[a] + nums[b] + nums[c] + nums[d] == target
+ *
+ *  You may return the answer in any order.
+ *
+ * Constraints:
+ *  1 <= nums.length <= 200
+ *  -10^9 <= nums[i] <= 10^9
+ *  -10^9 <= target <= 10^9
+ *
+ * Complexity:
+ *  Time:   O(n^3)
+ *    + 3 nested loops that iterate through the whole array
+ *    + while adding to the set is O(1), it takes O(n) to copy the set into a List
+ *  Space:  O(n^4)
+ *    - every single quadruple is valid and stored
+ */
 public class FourSum{
   public static List<List<Integer>> fourSum(int[] nums, int target){
-    return new ArrayList<>();
+    Set<List<Integer>> solution = new HashSet<>();
+    if (nums.length < 4){
+      // return empty list
+    }
+    else{
+      Arrays.sort(nums);
+      for (int i = 0; i < nums.length - 3; i++){
+        int a = nums[i];
+        for (int j = i + 1; j < nums.length - 2; j++){
+          int b = nums[j];
+          int start = j + 1;
+          int end = nums.length - 1;
+          while (start < end){
+            int c = nums[start];
+            int d = nums[end];
+            if (a + b + c + d == target){
+              solution.add(Arrays.asList(a, b, c, d));
+              start++;
+              end--;
+            }
+            else if (a + b + c + d > target){
+              end--;
+            }
+            else{// a + b + c + d < target
+              start++;
+            }
+          }
+        }
+      }
+    }
+    return new ArrayList<>(solution);
   }
 }
--- src/test/java/mgraca/medium/FourSumTest.java
+++ src/test/java/mgraca/medium/FourSumTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class FourSumTest{
   @Test
@@ -14,10 +16,11 @@ public class FourSumTest{
     int[] input = new int[]{1,0,-1,0,-2,2};
     int target = 0;
     List<List<Integer>> expectedOutput = new ArrayList<>();
-    expectedOutput.add(new ArrayList<>(Arrays.asList(-2,-1,1,2)));
-    expectedOutput.add(new ArrayList<>(Arrays.asList(-2,0,0,2)));
-    expectedOutput.add(new ArrayList<>(Arrays.asList(-1,0,0,1)));
+    expectedOutput.add(Arrays.asList(-2,-1,1,2));
+    expectedOutput.add(Arrays.asList(-2,0,0,2));
+    expectedOutput.add(Arrays.asList(-1,0,0,1));
     List<List<Integer>> actualOutput = FourSum.fourSum(input, target);
+    Collections.sort(actualOutput, new ListComparator<Integer>());
     String errorMsg = "Expected " + expectedOutput + ", returned " + actualOutput;
     assertTrue(errorMsg, actualOutput.equals(expectedOutput));
   }
@@ -27,9 +30,72 @@ public class FourSumTest{
     int[] input = new int[]{2,2,2,2,2};
     int target = 8;
     List<List<Integer>> expectedOutput = new ArrayList<>();
-    expectedOutput.add(new ArrayList<>(Arrays.asList(2,2,2,2)));
+    expectedOutput.add(Arrays.asList(2,2,2,2));
     List<List<Integer>> actualOutput = FourSum.fourSum(input, target);
     String errorMsg = "Expected " + expectedOutput + ", returned " + actualOutput;
     assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  @Test
+  public void example3(){
+    int[] input = new int[]{0,0,0,0};
+    int target = 0;
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    expectedOutput.add(Arrays.asList(0,0,0,0));
+    List<List<Integer>> actualOutput = FourSum.fourSum(input, target);
+    String errorMsg = "Expected " + expectedOutput + ", returned " + actualOutput;
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  @Test
+  public void example4(){
+    int[] input = new int[]{-2,-1,-1,1,1,2,2};
+    int target = 0;
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    expectedOutput.add(Arrays.asList(-2,-1,1,2));
+    expectedOutput.add(Arrays.asList(-1,-1,1,1));
+    List<List<Integer>> actualOutput = FourSum.fourSum(input, target);
+    Collections.sort(actualOutput, new ListComparator<Integer>());
+    String errorMsg = "Expected " + expectedOutput + ", returned " + actualOutput;
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  @Test
+  public void example5(){
+    int[] input = new int[]{-3,-1,0,2,4,5};
+    int target = 1;
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    expectedOutput.add(Arrays.asList(-3,-1,0,5));
+    List<List<Integer>> actualOutput = FourSum.fourSum(input, target);
+    Collections.sort(actualOutput, new ListComparator<Integer>());
+    String errorMsg = "Expected " + expectedOutput + ", returned " + actualOutput;
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  @Test
+  public void example6(){
+    int[] input = new int[]{-1,0,1,2,-1,-4};
+    int target = -1;
+    List<List<Integer>> expectedOutput = new ArrayList<>();
+    expectedOutput.add(Arrays.asList(-4,0,1,2));
+    expectedOutput.add(Arrays.asList(-1,-1,0,1));
+    List<List<Integer>> actualOutput = FourSum.fourSum(input, target);
+    Collections.sort(actualOutput, new ListComparator<Integer>());
+    String errorMsg = "Expected " + expectedOutput + ", returned " + actualOutput;
+    assertTrue(errorMsg, actualOutput.equals(expectedOutput));
+  }
+
+  // allows us to sort list of lists
+  private static class ListComparator<T extends Comparable<T>> implements Comparator<List<T>> {
+    @Override
+    public int compare(List<T> o1, List<T> o2) {
+      for (int i = 0; i < Math.min(o1.size(), o2.size()); i++) {
+        int c = o1.get(i).compareTo(o2.get(i));
+        if (c != 0) {
+          return c;
+        }
+      }
+      return Integer.compare(o1.size(), o2.size());
+    }
   }
 }
