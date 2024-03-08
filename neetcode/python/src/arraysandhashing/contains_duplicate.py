b558bd1d1889a5884b32a014c806ce47c5bf0457
Matthew Graca <matthew.graca@gmail.com>
2024-03-07 16:29:35 -0800

    added problem 1 python solution

--- neetcode/python/src/arraysandhashing/contains_duplicate.py
+++ neetcode/python/src/arraysandhashing/contains_duplicate.py
@@ -1,2 +1,14 @@
-def contains_duplicate():
-    return True
+from typing import List
+
+class Solution:
+    def containsDuplicate(nums: List[int]) -> bool:
+        a = set(nums)
+        return len(a) != len(nums)
+
+'''
+Basically, whenever I see the word 'duplicate', my immediate reaction is to 
+use the data structure that disallows dupes (set). Just convert the input into 
+a set, and it will automatically get rid of all dupes; so if the length of the 
+set is less than the length of nums, there must be dupes; else they are the 
+same, and there were no dupes.
+'''
--- neetcode/python/test/arraysandhashing/contains_duplicate_test.py
+++ neetcode/python/test/arraysandhashing/contains_duplicate_test.py
@@ -1,10 +1,23 @@
 import unittest
-from src.arraysandhashing import contains_duplicate
+from src.arraysandhashing.contains_duplicate import Solution 
 
 class ContainsDuplicateTest(unittest.TestCase):
     def test_example1(self):
-        actual = contains_duplicate.contains_duplicate()
+        arg = [1,2,3,1]
+        actual = Solution.containsDuplicate(arg)
         expected = True
+        self.assertEqual(actual, expected)
+
+    def test_example2(self):
+        arg = [1,2,3,4]
+        actual = Solution.containsDuplicate(arg)
+        expected = False 
+        self.assertEqual(actual, expected)
+
+    def test_example3(self):
+        arg = [1,1,1,3,3,4,3,2,4,2]
+        actual = Solution.containsDuplicate(arg)
+        expected = True 
         self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
