448c2190324f529290b9f3b555f5753980335383
Matthew Graca <matthew.graca@gmail.com>
2024-03-11 13:13:30 -0700

    updated comments to include time and space analysis

--- neetcode/python/src/arraysandhashing/contains_duplicate.py
+++ neetcode/python/src/arraysandhashing/contains_duplicate.py
@@ -11,4 +11,10 @@ use the data structure that disallows dupes (set). Just convert the input into
 a set, and it will automatically get rid of all dupes; so if the length of the 
 set is less than the length of nums, there must be dupes; else they are the 
 same, and there were no dupes.
+
+Suppose n is the size of the list of nums
+Time: n for initializing the set, 1 for comparing the list and set lengths
+    - O(n)
+Space: n for initializing the set
+    - O(n)
 '''
