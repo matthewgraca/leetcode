451990535ca47cfcb91f88665ff044f175c32c51
matthewgraca <mgraca@cpp.edu>
2024-04-01 15:09:32 -0700

    removed comments and cleaned up solution to one line

--- neetcode/python/src/arraysandhashing/contains_duplicate.py
+++ neetcode/python/src/arraysandhashing/contains_duplicate.py
@@ -2,19 +2,4 @@ from typing import List
 
 class Solution:
     def containsDuplicate(nums: List[int]) -> bool:
-        a = set(nums)
-        return len(a) != len(nums)
-
-'''
-Basically, whenever I see the word 'duplicate', my immediate reaction is to 
-use the data structure that disallows dupes (set). Just convert the input into 
-a set, and it will automatically get rid of all dupes; so if the length of the 
-set is less than the length of nums, there must be dupes; else they are the 
-same, and there were no dupes.
-
-Suppose n is the size of the list of nums
-Time: n for initializing the set, 1 for comparing the list and set lengths
-    - O(n)
-Space: n for initializing the set
-    - O(n)
-'''
+        return len(set(nums)) != len(nums)
