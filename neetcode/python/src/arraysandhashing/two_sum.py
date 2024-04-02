2684da99c5c7e431cb18c00a81d4967dbee1bb06
matthewgraca <mgraca@cpp.edu>
2024-04-01 17:04:41 -0700

    refactor and removed comments

--- neetcode/python/src/arraysandhashing/two_sum.py
+++ neetcode/python/src/arraysandhashing/two_sum.py
@@ -10,34 +10,9 @@ class Solution:
 
     def twoSum(nums: List[int], target: int) -> List[int]:
         dictNums = dict()
-        for i in range(0, len(nums)):
-            j = target - nums[i]
-            if j in dictNums:
-                return [dictNums[j], i]
+        for i in range(len(nums)):
+            search = target - nums[i]
+            if search in dictNums:
+                return [dictNums[search], i]
             dictNums[nums[i]] = i
-        return [0,0]
-
-'''
-The first run, we did the naive method of comparing every combination of two 
-numbers to find the target.
-
-The second run was a bit tricky. At first I thought to check only combinations 
-of the target; but it turns out that since the input can contain negative 
-numbers, the number of possible combinations is actually infinite.
-
-In order to limit the number of combinations I need to check, I got the 
-target and subtracted it with the current number I have -- then compare 
-that with a dictionary of all the numbers I've already checked. That way, 
-I'm only looking for 1 number at a time, for n numbers. Normally this would 
-be n^2, but with the magic of dictionaries, each search goes from n time to 
-1 time; so over n searches, it's just n instead of n^2.
-
-Time: n to create the dictionary, n to conduct n searches
-    - O(n)
-Space: n for the dictionary
-    - O(n)
-
-Pattern: I think the idea here is that when you are looking for combinations, 
-you can boil it down to a searching problem. So if you can identify what 
-exactly you are searching for, then you can make into a dictionary problem.
-'''
+        return [-1,-1]
