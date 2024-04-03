a88c9869465f2bc74aa5ed9ca5268219d650f0d2
matthewgraca <mgraca@cpp.edu>
2024-04-03 15:43:31 -0700

    removed comments

--- neetcode/python/src/arraysandhashing/group_anagrams.py
+++ neetcode/python/src/arraysandhashing/group_anagrams.py
@@ -13,22 +13,3 @@ class Solution:
         for s in dictStrs:
             ans.append(dictStrs[s])
         return ans
-
-'''
-This solution makes use of the fact that every anagram of a word is the same 
-string as the original if they are both sorted. With that in mind, we can 
-take every string in the list, sort it, then compare it to a dictionary of 
-strings. If it already exists, that string is put in the same group.
-
-Again, the basic idea is were going to do a lot of searching; in this case, 
-we sort the word and search our dictionary for the same string.
-
-Also, Python is annoyinggggg.
-
-Assuming n strings and each string has m characters
-Time: n*mlogm for sorting every string in the list, 1 for comparing that sorted 
-string with every key in the dictionary, n for constructing the solution.
-    - O(n*mlogm)
-Space: n for the dictionary, n for the solution
-    - O(n)
-'''
