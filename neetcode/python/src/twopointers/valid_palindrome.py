84fa109ad1969bcd4b4c4ca55c116e5bd5cdc7ca
matthewgraca <mgraca@cpp.edu>
2024-04-08 12:34:12 -0700

    removed comments, refactor variable names for two pointer clarity

--- neetcode/python/src/twopointers/valid_palindrome.py
+++ neetcode/python/src/twopointers/valid_palindrome.py
@@ -1,41 +1,17 @@
-'''
-palindrome:
-    1. convert all uppercase letters into lowercase letters
-    2. remove all non-alphanumeric characters
-    3. reads the same forward and backward (so only letters and numbers)
-
-checking for palindrome:
-two pointers, one at each end of the string, check if they're the same character.
-End once the two pointers converge at the middle of the string.
-'''
 class Solution:
     def isPalindrome(s: str) -> bool:
         # clean the string to only contain alphanumeric characters
-        cleanedS = str()
+        cleanS = str()
         for c in s:
             if c.isalnum():
-                cleanedS += c.lower()
+                cleanS += c.lower()
 
         # check for palindrome
-        i = 0
-        j = len(cleanedS) - 1
-        while i < j:
-            if cleanedS[i] != cleanedS[j]:
+        left = 0
+        right = len(cleanS) - 1
+        while left < right:
+            if cleanS[left] != cleanS[right]:
                 return False
-            i += 1
-            j -= 1
+            left += 1
+            right -= 1
         return True
-
-'''
-wow I banged out this solution in 15 mins I feel so cool.
-
-Nothing much to say here, but knowing the trick is that with palindromes, 
-you are checking one side of an array with the other side; making it a 
-clear choice to use two pointers to check one side while the other pointer 
-checks the other.
-
-Time: n to clean the string, n/2 to check the palindrome
-    - O(n)
-Space: n to create a cleaned string
-    - O(n)
-'''
