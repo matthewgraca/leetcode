aeb8d939440783178d6415cd1c0a7a86057e1b18
matthewgraca <mgraca@cpp.edu>
2024-04-11 14:42:24 -0700

    refactored to use for loop to emphasize constant right pointer incrementing

--- neetcode/python/src/slidingwindow/longest_substring.py
+++ neetcode/python/src/slidingwindow/longest_substring.py
@@ -1,15 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(s: str) -> int:
-        left, right = 0, 0
+        left = 0
         substr = set()
         substrLength = 0
-        while right < len(s):
+        for right in range(len(s)):
             # if a duplicate is found, move left until it is removed
             while s[right] in substr:
                 substr.remove(s[left])
                 left += 1
             # add character to valid set, update length
             substr.add(s[right])
-            right += 1
-            substrLength = max(substrLength,right-left)
+            substrLength = max(substrLength,right-left+1)
         return substrLength 
--- neetcode/python/src/slidingwindow/stocks.py
+++ neetcode/python/src/slidingwindow/stocks.py
@@ -1,12 +1,11 @@
 from typing import List
 class Solution:
     def maxProfit(prices: List[int]) -> int:
-        left, right = 0, 1
+        left = 0
         profit = 0
-        while right < len(prices):
+        for right in range(len(prices)):
             # move the left ptr if the right catches a smaller value to buy at
             if prices[right] < prices[left]:
                 left = right
             profit = max(profit,prices[right]-prices[left])
-            right += 1
         return profit
