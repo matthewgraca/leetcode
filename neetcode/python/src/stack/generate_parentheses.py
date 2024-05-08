66051fba82a3145466803608f2f27a6f9357e593
matthewgraca <mgraca@cpp.edu>
2024-05-08 16:33:32 -0700

    refactored to dfs for clearer naming, removed comments

--- neetcode/python/src/stack/generate_parentheses.py
+++ neetcode/python/src/stack/generate_parentheses.py
@@ -1,36 +1,25 @@
 from typing import List
 
 class Solution:
-    def __init__(self):
-        pass
-
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []    # contains full solutions
-        stack = []  # string containing parens
-        self.backtrack(0, 0, n, stack, res)
+        st = ""
+        res = []
+        self.dfs(n, 0, 0, st, res)
         return res
-
-    def backtrack(self, openCount, closedCount, n, stack, res):
-        # leaf - base case: combination found
-        if openCount == n and closedCount == n:
-            res.append("".join(stack))
         
-        # branch 1: open paren is addable
-        if openCount < n:
-            stack.append("(")
-            self.backtrack(openCount+1, closedCount, n, stack, res)
-            stack.pop()
-        # branch 2: closed paren is addable 
-        if closedCount < openCount:
-            stack.append(")")
-            self.backtrack(openCount, closedCount+1, n, stack, res)
-            stack.pop()
+    def dfs(self, n: int, opens: int, closes: int, st: str, res: List[str]) -> None:
+        # base case: all parens added and matched
+        if len(st) == 2*n:
+            res.append(st)
+            return
 
+        # left branch: add open parens
+        if opens < n:
+            self.dfs(n, opens+1, closes, st + "(", res)
+        
+        # right branch: add matching closed parens
+        if opens > closes:
+            self.dfs(n, opens, closes+1, st + ")", res)
 
-'''
-silly ass trick, the "stack" idea is using the function 
-call stack for a backtracking algorithm along with this actual parenthesis stack
-
-time : O(2^(2*n)) = O(4^n), but prunes a ton of solutions since we don't traverse the entire space
-space: stack and result are linear; O(n)
-'''
+        # can't make well-formed parentheses; backtrack
+        return
