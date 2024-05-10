7bf1e72a23aadc0145570875a4774f437092b4d1
matthewgraca <mgraca@cpp.edu>
2024-05-09 17:55:34 -0700

    updated comments for clarity

--- neetcode/python/src/stack/car_fleet.py
+++ neetcode/python/src/stack/car_fleet.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
     def carFleet(target: int, position: List[int], speed: List[int]) -> int:
         # map each car to their position, then sort by position
-        # this way, the end of the stack contains the car closest to the finish
+        # such that the top of the stack contains the car closest to the finish
         sortedPosition = sorted((pos, car) for (car, pos) in enumerate(position))
         fleets, timeToBeat = 0, 0
 
@@ -12,8 +12,8 @@ class Solution:
             pos, car = sortedPosition.pop()
             timeToFinish = (target - pos) / speed[car]
 
-            # if this time is worse than the previous car, this car will be in a new fleet
-            # and, it'll be the new "time to beat" for the next car to merge into this new fleet
+            # if can't catch the front car, add a fleet
+            # and, it'll be the new "time to beat" for the next car 
             if timeToFinish > timeToBeat:
                 fleets += 1
                 timeToBeat = timeToFinish
