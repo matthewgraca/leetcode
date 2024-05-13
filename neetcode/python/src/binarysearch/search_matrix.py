48c6a1cbd1af8eb9f3a22fe8766d9e93aba28ee5
matthewgraca <mgraca@cpp.edu>
2024-05-13 15:37:58 -0700

    refactored and removed notes

--- neetcode/python/src/binarysearch/search_matrix.py
+++ neetcode/python/src/binarysearch/search_matrix.py
@@ -4,35 +4,48 @@ class Solution:
     def __init__(self):
         pass
 
-    '''
-    same as binary search for a single dimension array, just dealing with annoying indexing
-    alternatively, instead of silly indexing, we can just use the classic binary search
-    while using a for loop for each row, since it's fundamentally an array of arrays
-    '''
-    def searchMatrix(self, matrix: List[int], target: int) -> bool:
-        for m in range(len(matrix)):
-            targetFound = self.recursive_search(matrix[m], target, 0, len(matrix[m])-1)
-            if targetFound:
-                return True
+    # search target in row, then refine search for target in columns
+    def searchMatrixRowThenCol(self, matrix: List[List[int]], target: int) -> bool:
+        lenRows, lenCols = len(matrix), len(matrix[0])
+        l, r = 0, lenRows-1
+
+        # binary search for row containing target
+        while l <= r:
+            midRow = (l + r) // 2
+
+            # if row contains the target, binary search for target 
+            if target <= matrix[midRow][-1] and target >= matrix[midRow][0]:
+                l, r = 0, lenCols-1
+                while l <= r:
+                    midCol = (l + r) // 2
+                    if target < matrix[midRow][midCol]:
+                        r = midCol-1
+                    elif target > matrix[midRow][midCol]:
+                        l = midCol+1
+                    else:
+                        return True
+                # target not found in any column
+                return False
+                
+            # target not contained in this row; determine if bianry search moves up or down
+            if target < matrix[midRow][-1]:
+                r = midRow-1
+            else: # target > matrix[midRow][0]
+                l = midRow+1
+        # target not contained in any row
         return False
 
-    def recursive_search(self, nums: List[int], target: int, start: int, end: int) -> bool:
-        mid = (end + start) // 2
-        # base case 1: targe not found
-        if start > end:
-            return False 
-        # base case 2: target found
-        if nums[mid] == target:
-            return True 
-
-        # left branch: target is in upper array
-        if target > nums[mid]:
-            return self.recursive_search(nums, target, mid+1, end)
-
-        # right branch: target is in lower array
-        if target < nums[mid]:
-            return self.recursive_search(nums, target, start, mid-1)
-'''
-time : given mxn range, logn to search each m for a total of mlogn
-space: no extra used; O(1)
-'''
+    # tricky indexing
+    def searchMatrixRaw(self, matrix: List[List[int]], target: int) -> bool:
+        lenRows, lenCols = len(matrix), len(matrix[0])
+        l, r = 0, lenRows*lenCols-1
+        while l <= r:
+            mid = (r + l) // 2
+            row, col = mid//lenCols, mid%lenCols
+            if target > matrix[row][col]:
+                l = mid+1
+            elif target < matrix[row][col]:
+                r = mid-1
+            else:
+                return True
+        return False
