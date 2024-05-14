8c2c2302c582754bcc16572e9de630f7f446a4f6
matthewgraca <mgraca@cpp.edu>
2024-05-14 14:44:20 -0700

    more refactor

--- neetcode/python/src/binarysearch/min_in_rotated_sorted_array.py
+++ neetcode/python/src/binarysearch/min_in_rotated_sorted_array.py
@@ -5,22 +5,16 @@ class Solution:
         pass
 
     def findMin(self, nums: List[int]) -> int:
-        # binary search with special conditionals woooo
         l, r = 0, len(nums)-1
         while l <= r:
             m = (r + l) // 2
             left, mid, right = nums[l], nums[m], nums[r]
 
-            # case 1: continuous (strictly increasing)
-            if left < right:
-                r = m-1
-            # case 2: discontinuous
+            # case 1: min value in left; trim right (mid may be min itself)
+            if mid < right:
+                r = m
+            # case 2: min value in right; trim left (mid > right; can't be min)
             else:
-                # case a: min value in left; trim right (mid may be min itself)
-                if mid < right:
-                    r = m
-                # case b: min value in right; trim left
-                else:
-                    l = m+1
+                l = m+1
         
         return left
