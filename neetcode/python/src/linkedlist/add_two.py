883dae509e3b4e00b87b231b2ace4f5a7d4a1c23
matthewgraca <mgraca@cpp.edu>
2024-05-21 15:15:37 -0700

    refactored

--- neetcode/python/src/linkedlist/add_two.py
+++ neetcode/python/src/linkedlist/add_two.py
@@ -2,23 +2,24 @@ from src.linkedlist.ListNode import ListNode
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        temp1, temp2 = l1, l2
-        head = temp3 = ListNode(0)
+        dummy = temp = ListNode(-1)
         carry = 0
-        while temp1 or temp2 or carry:
-            # grab values from both lists
-            op1, op2 = 0, 0
-            if temp1:
-                op1 = temp1.val
-                temp1 = temp1.next
-            if temp2:
-                op2 = temp2.val
-                temp2 = temp2.next
 
-            # add 
-            val = op1 + op2 + carry
-            temp3.next = ListNode(val % 10)
-            carry = val  // 10
-            temp3 = temp3.next
+        # take from l1 and l2
+        while l1 or l2 or carry:
+            # pull values from l1 and/or l2
+            val1, val2 = 0, 0
+            if l1:
+                val1 = l1.val
+                l1 = l1.next
+            if l2:
+                val2 = l2.val
+                l2 = l2.next
+            
+            # perform sum
+            sumval = (val1 + val2 + carry) % 10
+            carry = (val1 + val2 + carry) // 10
+            temp.next = ListNode(sumval)
+            temp = temp.next
 
-        return head.next
+        return dummy.next   
