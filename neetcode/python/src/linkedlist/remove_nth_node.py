febfde148c35bbdf1d69fb0d56b5023c483aaab9
matthewgraca <mgraca@cpp.edu>
2024-05-20 17:00:40 -0700

    refactored

--- neetcode/python/src/linkedlist/remove_nth_node.py
+++ neetcode/python/src/linkedlist/remove_nth_node.py
@@ -4,43 +4,22 @@ class Solution:
     def __init__(self):
         pass
 
-    # no stack, single pass, slow-fast ptr solution
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        # set up a slow pointer that reaches the n+1th node when fast hits the end
-        dummy = ListNode(0, head) 
-        slow = dummy
-        fast = head
-        for i in range(n):
-            fast = fast.next
-        while fast:
-            slow = slow.next
-            fast = fast.next
+        # have slow be 1 slower than fast
+        dummy = ListNode(-1, head)
+        slow, fast = dummy, head
 
-        # remove nth node by stitching n+1 to n-1
-        slow.next = slow.next.next
+        # move fast to nth from beginning
+        while n > 0:
+            fast = fast.next
+            n -= 1
         
-        # head may be removed; return dummy.next
-        return dummy.next 
+        # move slow until fast hits end of list
+        # slow will be n+1 from the end
+        while fast:
+            slow, fast = slow.next, fast.next
+        
+        # connect
+        slow.next = slow.next.next
 
-    # stack-based, iterative solution
-    def removeNthFromEnd1(self, head: ListNode, n: int) -> ListNode:
-        # create a stack that contains all nodes
-        stack = []
-        temp = head
-        while temp:
-            stack.append(temp)
-            temp = temp.next
-
-        # pop n items from stack; curr will have nth node from the end
-        curr = None
-        for i in range(n):
-            curr = stack.pop()
-
-        # top of stack has previous node
-        if stack:
-            prev = stack[-1]
-            prev.next = curr.next
-        else:
-            head = head.next
-
-        return head
+        return dummy.next
