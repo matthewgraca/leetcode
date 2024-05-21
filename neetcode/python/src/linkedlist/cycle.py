93cb7486dcbbdc38e7a6f0923f8e4d6d06a90758
matthewgraca <mgraca@cpp.edu>
2024-05-21 15:46:07 -0700

    refactored

--- neetcode/python/src/linkedlist/cycle.py
+++ neetcode/python/src/linkedlist/cycle.py
@@ -5,34 +5,12 @@ class Solution:
         pass
 
     def hasCycle(self, head: ListNode) -> bool:
-        # empty list implies no cycle
-        if head is None:
-            return False
-
-        slow, fast = head, head.next
-        while fast:
-            # fast catches slow implies cycle
-            if fast is slow:
-                return True
-            # fast hits null implies no cycle
-            if fast.next is None:
-                return False 
-            # move ptrs; keep fast at 2x speed of slow
+        slow = fast = head
+        # if the fast pointer catches the slow pointer, a cycle exists
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        # if this is reached, fast hit null; implies no cycle
+            if slow == fast:
+                return True
+        
         return False
-
-    # stuff every node into a set. if the next node points to a node in the set, there's a cycle
-    def hasCycleSet(self, head: ListNode) -> bool:
-        # if there's no cycle, temp will eventually be null
-        # if there's a cycle, we'll eventually hit a dupe, returning true 
-        nodes = set()
-        temp = head
-        while temp:
-            if temp in nodes:
-                return True 
-            else:
-                nodes.add(temp)
-                temp = temp.next
-        return False 
