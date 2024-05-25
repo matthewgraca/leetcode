bbedbafe1901293ce2e759f6e6aad15b22e53b24
matthewgraca <mgraca@cpp.edu>
2024-05-25 13:46:43 -0700

    slight refactor, removed notes

--- neetcode/python/src/linkedlist/reverse_nodes_in_k_group.py
+++ neetcode/python/src/linkedlist/reverse_nodes_in_k_group.py
@@ -5,50 +5,35 @@ class Solution:
         pass
 
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        return self.reverseList(head, k, k)
-
-    # scans ahead to check if a valid k-group exists
-    def kGroupFound(self, curr: ListNode, k: int) -> ListNode:
-        count = 1
-        temp = curr
-        while temp.next and count < k:
-            temp = temp.next
-            count += 1
-        return True if count == k else False
-
-    def reverseList(self, head: ListNode, count: int, k: int) -> ListNode:
-        # base case: invalid k-group
-        # if no valid k-group is ahead, do not reverse
-        if not self.kGroupFound(head, k):
+        return self.reverseGroup(head, k, k)
+    
+    # recursively reverses every k-group in the list
+    def reverseGroup(self, head: ListNode, count: int, k: int) -> ListNode:
+        # base case: no k-group, return
+        if not self.validKGroup(head, k):
             return head
 
-        # reverse current k-group
-        prev = None
-        curr = head
-        after = None
-        while curr and count > 0:
+        # recursive step: 
+        # 1. reverse the current group,
+        prev, curr, after = None, head, None
+        while count:
             after = curr.next
             curr.next = prev
-            prev = curr 
-            curr = after
+            prev, curr = curr, after
             count -= 1
 
-        # if there's another possible k-group, recurse
+        # 2. then reverse the next k-group 
         if after:
-            head.next = self.reverseList(after, k, k)
-
+            head.next = self.reverseGroup(after, k, k)
+        
+        # once all lists reversed, return the head of the original reversed list
         return prev
 
-'''
-like this solution b/c we actually repurpose code
-also takes advantage of the iterative/recursive solution;
-prev will be the head of the reversed list, but head remained
-the head of the old list -- since the list is reversed, that means
-the head is now the tail, so we can actually attach it to the new group
-via recursion
-
-time: o(n)
-    - n to reverse the list
-    - n to scout and check if groups are valid
-space: O(1)
-'''
+    # checks if the given list has a valid k-group
+    def validKGroup(self, head: ListNode, k: int) -> bool:
+        temp = head
+        while temp and k:
+            temp = temp.next
+            k -= 1
+        # if the list ends before k hits 0, this is an invalid k-group
+        return True if k == 0 else False
