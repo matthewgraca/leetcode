11be10bca5c58dc5b3156f46b409223f665d33eb
matthewgraca <mgraca@cpp.edu>
2024-06-25 15:54:26 -0700

    refactored and removed comments

--- neetcode/python/src/trees/max_depth_of_binary_tree.py
+++ neetcode/python/src/trees/max_depth_of_binary_tree.py
@@ -6,37 +6,7 @@ class Solution:
 
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
-            return 0 
-
-        st = [(root,1)]
-        maxDepth = 0
-        while st:
-            node, depth = st.pop()
-            maxDepth = max(maxDepth, depth)
-            if node.left:
-                st.append((node.left, depth+1))
-            if node.right:
-                st.append((node.right, depth+1))
-
-        return maxDepth 
-
-
-    def maxDepthR(self, root: TreeNode) -> int:
-        if root is None:
             return 0
 
-        leftTreeDepth = 1 + self.maxDepthR(root.left)
-        rightTreeDepth = 1 + self.maxDepthR(root.right)
-
-        return max(leftTreeDepth, rightTreeDepth)
-
-'''
-Easy to think of this as a mathematical object...
-in fact, anything that you pass in that doesn't have state can be thought kinda like this, no?
-T(n) = 1 + T(n-1) + 1 + T(n-1) = 2 + 2T(n-1)
-
-Time:
-    O(n); traverse n nodes
-Space:
-    O(n), for degenerate binary trees
-'''
+        # postorder traversal, save only deepest value
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
