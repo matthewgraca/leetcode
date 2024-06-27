b8a743d06376200f23e6f5dba1179887df7a2615
Matthew Graca <matthew.graca@gmail.com>
2024-06-26 17:14:45 -0700

    refactored and removed comments

--- neetcode/python/src/trees/diameter_of_binary_tree.py
+++ neetcode/python/src/trees/diameter_of_binary_tree.py
@@ -4,31 +4,23 @@ class Solution:
     def __init__(self):
         pass
 
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        maxDepth, diameter = self.maxDepthAndDiameter(root)
+    def diameterOfBinaryTree(self, root: TreeNode)-> int:
+        _, diameter = self.depthAndDiameter(root)
         return diameter
 
-    def maxDepthAndDiameter(self, root: TreeNode) -> (int, int):
-        if root is None:
+    def depthAndDiameter(self, root: TreeNode) -> (int, int):
+        if not root:
             return 0, 0
+        
+        # get current max depth and diameter of this node
+        leftTreeMaxDepth, leftTreeDiameter = self.depthAndDiameter(root.left)
+        rightTreeMaxDepth, rightTreeDiameter = self.depthAndDiameter(root.right)
 
-        # get the depth and diameter of the left tree
-        leftTreeDepth, leftTreeDiameter = self.maxDepthAndDiameter(root.left)
-        # get the depth and diameter of the right tree
-        rightTreeDepth, rightTreeDiameter = self.maxDepthAndDiameter(root.right)
-        # sum left and right tree depth at current moment for a current diameter
-        currentDiameter = leftTreeDepth + rightTreeDepth
-        # the max diameter is the largest diameter of the three
-        maxDiameter = max(leftTreeDiameter, rightTreeDiameter, currentDiameter)
+        # calculate new max depth and diameter
+        maxDepth = max(leftTreeMaxDepth, rightTreeMaxDepth) + 1
+        # note : we don't add 1 b/c of the definition of diameter counts edges, not nodes
+        currDiameter = leftTreeMaxDepth + rightTreeMaxDepth
+        maxDiameter = max(leftTreeDiameter, rightTreeDiameter, currDiameter)
 
-        return max(leftTreeDepth, rightTreeDepth) + 1, maxDiameter 
-'''
-The idea is the diameter of a binary tree is the sum of the max left tree depth and the max right tree depth.
-    - this means we need to save both the max depth of the current tree, and the total diameter at this moment.
-
-Some solutions opt to use a global diameter state, but i want this to be as stateless as possible since leetcode may create an object out of this class, essentially reusing diameter, which saves the last largest diameter of a different tree.
-
-complexity:
-    time: O(n) for n nodes
-    space: O(n) for n nodes (for a degenerate binary tree)
-'''
+        # pass those values up during post-order traversal
+        return maxDepth, maxDiameter
