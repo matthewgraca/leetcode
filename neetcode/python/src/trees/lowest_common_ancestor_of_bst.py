b2b35a427a7a629f8bc707bda1249e4c6f752def
Matthew Graca <matthew.graca@gmail.com>
2024-06-27 16:13:47 -0700

    removed comments, slight refactor, and added a recursive solution

--- neetcode/python/src/trees/lowest_common_ancestor_of_bst.py
+++ neetcode/python/src/trees/lowest_common_ancestor_of_bst.py
@@ -4,25 +4,35 @@ class Solution:
     def __init__(self):
         pass
 
-    def lowestCommonAncestor(
-            self, root: TreeNode, p: TreeNode, q: TreeNode
+    def lowestCommonAncestorIt(
+        self, root: TreeNode, p: TreeNode, q: TreeNode
     ) -> TreeNode:
         # binary search for p and q
         while True:
-            # if both p and q in left subtree, go there
+            # if p and q are left, go left
             if p.val < root.val and q.val < root.val:
                 root = root.left
-            # elif both p and q in right subree, go there
+            # if p and q are right, go right
             elif p.val > root.val and q.val > root.val:
                 root = root.right
-            # a node was found, so the other must be a child of this node
-            # or one is in the left tree and the other is in the right tree
+            # if p and q are in between root (or one is equal to root), return
             else:
                 return root
-'''
-got sent to implementation hell because i forgot this was a BINARY SEARCH TREE
-time:
-    binary search for 2 valules, but done at the same time so just lgn
-space:
-    1
-'''
+
+    def lowestCommonAncestor(
+        self, root: TreeNode, p: TreeNode, q: TreeNode
+    ) -> TreeNode:
+        # p and q guaranteed to be in tree, so no real base case needed
+
+        # recursive step:
+        # go left if p and q are left
+        if p.val < root.val and q.val < root.val:
+            root = self.lowestCommonAncestor(root.left, p, q)
+        
+        # go right if p and q are right
+        if p.val > root.val and q.val > root.val:
+            root = self.lowestCommonAncestor(root.right, p, q)
+        
+        # when lca is found, return root 
+        # ((p>root>q) or (p<root<q) or (p=root or q=root))
+        return root
