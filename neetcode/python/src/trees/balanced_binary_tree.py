19efec0b54f9eebe7af95a662bd49a2f093c3bce
Matthew Graca <matthew.graca@gmail.com>
2024-06-26 17:14:34 -0700

    refactored and removed comments

--- neetcode/python/src/trees/balanced_binary_tree.py
+++ neetcode/python/src/trees/balanced_binary_tree.py
@@ -5,31 +5,19 @@ class Solution:
         pass
 
     def isBalanced(self, root: TreeNode) -> bool:
-        maxDepth, isTreeBalanced = self.maxDepth(root)
-        return isTreeBalanced
+        _, isBalanced = self.maxDepth(root)
+        return isBalanced
 
     def maxDepth(self, root: TreeNode) -> (int, bool):
         if not root:
             return 0, True
-
-        # get depth of left and right tree, along with if it was balanced
-        leftTreeDepth, isLeftBalanced = self.maxDepth(root.left)
-        rightTreeDepth, isRightBalanced = self.maxDepth(root.right)
-
-        # check the current balance
-        isCurrBalanced = False if abs(leftTreeDepth - rightTreeDepth) > 1 else True
-
-        # tree is balanced only if it was, and is, balanced (at every node)
-        isTotalBalanced = isLeftBalanced and isRightBalanced and isCurrBalanced
-
-        return max(leftTreeDepth+1, rightTreeDepth+1), isTotalBalanced
         
-'''
-The definition of a balanced tree is that it is "height-balanced"; i.e., the depth 
-of two subtrees of EVERY node never differs by more than one.
-    - so it's not just comparing the left and right subtree from the root;
-        you have to compare the left and right subtree depth at EVERY node
+        leftMaxDepth, isLeftTreeBalanced = self.maxDepth(root.left)
+        rightMaxDepth, isRightTreeBalanced = self.maxDepth(root.right)
 
-So the idea is to get the left subtree depth and the rigth subtree depth for each node. If the 
-difference is more than 1, return false
-'''
+        # only balanced if all subtrees are balanced as well
+        isBalanced = isLeftTreeBalanced and isRightTreeBalanced
+        if abs(leftMaxDepth - rightMaxDepth) > 1:
+            isBalanced = False
+
+        return 1 + max(leftMaxDepth, rightMaxDepth), isBalanced
