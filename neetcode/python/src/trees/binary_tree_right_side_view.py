596ce3cd139914080ade570564c33a24858947bc
Matthew Graca <matthew.graca@gmail.com>
2024-06-28 16:24:18 -0700

    refactored and removed comments

--- neetcode/python/src/trees/binary_tree_right_side_view.py
+++ neetcode/python/src/trees/binary_tree_right_side_view.py
@@ -7,47 +7,21 @@ class Solution:
         pass
 
     def rightSideView(self, root: TreeNode) -> List[int]:
-        res = []
         if not root:
-            return res 
-
-        # level-order traversal, but nodes are saved from right -> left
-        q = deque()
-        q.append(root)
+            return []
+        
+        # level order traversal
+        res = []
+        q = deque([root])
         while q:
-            currLevel = deque()
-            while q:
-                currLevel.append(q.popleft())
-
-            res.append(currLevel[0].val) # rightmost value on the current level 
-            while currLevel:
-                node = currLevel.popleft()
+            # top of queue will contain rightmost node on the current level
+            res.append(q[0].val)
+            nodesInLevel = len(q)
+            for _ in range(nodesInLevel):
+                node = q.popleft()
                 if node.right:
                     q.append(node.right)
                 if node.left:
                     q.append(node.left)
 
         return res
-'''
-Seems like dfs but you're going to the right instead of classically, the left
-Straight up ignore all the left nodes? we'll see with tests.
-
-OK this is a bit freaky; it's more like taking the binary tree and rotating 
-it so that it lays on the left side. So if there are ONLY left nodes and no 
-right nodes, you'll still "see" the left nodes.
-
-this seems more like bfs; if there is a right node, add that to the list;
-if there is no right node and there is a left node, add that to the list.
-
-so we start off with classic bfs and attempt to add this extra conditional 
-that allows us to track right nodes; BUT track left nodes if there is no right node
-in the way.
-
-More specifically, this seems more like level-order traversal but keeping the 
-rightmost node value.
-
-time:
-    O(n) to traverse n nodes for order-level traversal
-space:
-    O(1) for only constant space used, not including output space
-'''
