f64f5adcb34c06b921af9018bc2d7f3b74888da9
Matthew Graca <matthew.graca@gmail.com>
2024-07-09 16:14:27 -0700

    added subset backtracking solution

--- neetcode/python/src/backtracking/subsets.py
+++ neetcode/python/src/backtracking/subsets.py
@@ -5,19 +5,47 @@ class Solution:
         pass
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for i in range(len(nums)):
-            res.append([nums[i]])
-            self.backtrack(i, nums, [], res)
+        res = []
+        self.backtrack(0, nums, [], res)
         return res
 
-    def backtrack(self, i: int, nums: List[int], sol: List[int], res: List[List[int]]) -> None:
-        # base case: end of nums reached
+    def backtrack(
+        self, 
+        i: int, 
+        nums: List[int], 
+        subset: List[int], 
+        res: List[List[int]]
+    ) -> None:
+        # base case: valid subset
         if i >= len(nums):
-            res.append(sol)
+            res.append(subset.copy())
             return
-        
-        # left branch: add from nums
-        res.append(sol)
-        self.backtrack(i+1, nums, sol + [nums[i]], res)
 
+        # preorder dfs build solution
+        subset.append(nums[i])
+
+        # left step condition:
+        self.backtrack(i + 1, nums, subset, res)
+        subset.pop() # left tree end; pop and go to right tree
+        # right step condition:
+        self.backtrack(i + 1, nums, subset, res)
+        return
+'''
+Ideas around backtracking:
+- basically, it is a dfs traversal over a problem that requires generating combinations
+    - so think of a the problem as a tree where each node is a combination
+    - you just dfs through; (post order or pre order?)
+        - i think it's preorder since we pass changed data through as a function argument, so 
+            that means changes were made before the function call
+- some key ideas:
+    - base case: this is when you've reached a valid solution
+    - conditionals for left/right movement
+        - you encase the left/right recursive step in a conditional -- 
+            - if the condition is not met, that means no more solutions can be made down that 
+                path, so go right. if that path eventually closes, you'll hit the end of the func. 
+
+curr problem
+- pre order dfs traversal. subset is built in preorder dfs manner, res is built in postorder manner.
+- subset is itself a stack. since we pass the reference it doesn't respond to recursion, so we have 
+    to pop it so it follows the recursive calls.
+'''
