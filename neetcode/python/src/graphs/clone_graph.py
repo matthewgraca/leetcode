10f5e725a766f748a96e6d660b5c99252fbe5c17
Matthew Graca <matthew.graca@gmail.com>
2024-07-23 14:18:19 -0700

    added cloned graph solution

--- neetcode/python/src/graphs/clone_graph.py
+++ neetcode/python/src/graphs/clone_graph.py
@@ -5,4 +5,26 @@ class Solution:
         pass
 
     def cloneGraph(self, node: Node) -> Node:
-        return Node()
+        # maps old graph to copied graph nodes
+        originalToCopy = {}
+        return self.dfs(node, originalToCopy) if node else None
+
+    def dfs(self, node: Node, originalToCopy: dict) -> Node:
+        # if already visited, backtrack
+        if node in originalToCopy:
+            return originalToCopy[node]
+
+        # preorder traversal - make copy
+        copy = Node(node.val)
+        originalToCopy[node] = copy
+
+        # dfs
+        for neighbor in node.neighbors:
+            copy.neighbors.append(self.dfs(neighbor, originalToCopy))
+
+        return copy
+
+'''
+This looks like copy list with random pointer;
+needing a hashmap to follow how each node points to others
+'''
--- neetcode/python/test/graphs/clone_graph_test.py
+++ neetcode/python/test/graphs/clone_graph_test.py
@@ -1,6 +1,7 @@
 import unittest
 from src.graphs.clone_graph import Solution
+from src.graphs.node import Node
 
 class CloneGraphTest(unittest.TestCase):
     def test_example1(self):
-        
+        self.assertTrue(True)
