b055e79403cbcc9823d24fa54a4330902fcb8dd6
Matthew Graca <matthew.graca@gmail.com>
2024-07-22 17:17:32 -0700

    skeleton code for cloning graph problem

---
+++ neetcode/python/src/graphs/clone_graph.py
@@ -0,0 +1,8 @@
+from src.graphs.node import Node
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def cloneGraph(self, node: Node) -> Node:
+        return Node()
---
+++ neetcode/python/test/graphs/clone_graph_test.py
@@ -0,0 +1,6 @@
+import unittest
+from src.graphs.clone_graph import Solution
+
+class CloneGraphTest(unittest.TestCase):
+    def test_example1(self):
+        
