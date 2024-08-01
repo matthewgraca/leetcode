aa205fd6fe5a02e44b28f1780b872e18275e4c08
Matthew Graca <matthew.graca@gmail.com>
2024-07-31 18:13:39 -0700

    remove comments

--- neetcode/python/src/backtracking/palindrome_partitioning.py
+++ neetcode/python/src/backtracking/palindrome_partitioning.py
@@ -28,6 +28,8 @@ class Solution:
                 partition.append(s[i : j + 1])
                 self.dfs(s, j + 1, partition, res)
                 partition.pop()
+            # if not, check substring adding next character
+        # if no character can make a palindrome, backtrack
         return
 
     def isPalindrome(self, s: str, lo: int, hi: int):
@@ -36,34 +38,3 @@ class Solution:
                 return False
             lo, hi = lo + 1, hi - 1
         return True
-'''
-consider every subtring of s:
-    -"aab" -> "aab", "aa", "ab", "a", "a", "b"
-A partition of s makes up one group:
-    -"aab" partitions into -> ["aab"], ["aa", "b"], ["a", "ab"], ["a", "b", "b"]
-The goal is to find every group whose elements are palindromic:
-    -which is: ["aa", "b"], ["a", "b", "b"]
-
-The trace:
-start loop
-    - check a. is palindrome, add to partition
-        - check a. is palindrome, add to partition
-            - check b. is palindrome, add to partition
-                - end of s reached, add partitions to res
-                - b is popped
-                - end of loop. backtrack
-            - a is popped.
-            - end of loop. backtrack
-        - check ab. is not palindrome
-            - end of loop. backtrack
-        - end of loop. backtrack
-    - check aa. is palindrome, add to partition
-        -check b. is palindrome, add to partition
-            -end of s reached; add partition to res
-            - b is popped
-            - end of loop. backtrack
-        - end of loop. backtrack
-    - check aab. is not palindrome. backtrack
-    - end of loop
-end loop
-'''
