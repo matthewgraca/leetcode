46c473f8eb025512d2cb831b8325c32bf558e2d4
Matthew Graca <matthew.graca@gmail.com>
2024-08-07 16:48:13 -0700

    removed comments

--- neetcode/python/src/graphs/islands_and_treasure.py
+++ neetcode/python/src/graphs/islands_and_treasure.py
@@ -48,26 +48,3 @@ class Solution:
             (row, col) not in visited and
             grid[row][col] != waterCell and grid[row][col] != treasureCell 
         )
-
-'''
-bfs to find closest treasure cell from each land cell
-
-to flip this, we run bfs from the treasure and mark each
-land cell by +1 after each level of bfs
-
-I thought that you would need to pick up the min distance and mess with 
-the visited set, but it turns out that since the bfs occurs from all the 
-treasure cells at the same time, the searches end up "meeting" each other,
-instead of one overlapping the other search. so there's no need to save the 
-smallest distance since the searches are done concurrently 
-
-Let mn be the size of the grid
-time: 
-    - mn to search for treasure cells and push relevant cells into queue
-    - worst case mn time searching every cell
-    - O(mn)
-space:
-    - queue worst case becomes mn size
-    - visiting every node means set becomes mn size
-    - O(mn)
-'''
