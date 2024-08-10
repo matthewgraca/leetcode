db0a87700aab2d03ca86f7445f66db080e0df3c4
Matthew Graca <matthew.graca@gmail.com>
2024-08-09 17:45:00 -0700

    refactored and removed comments

--- neetcode/python/src/graphs/course_schedule.py
+++ neetcode/python/src/graphs/course_schedule.py
@@ -6,10 +6,8 @@ class Solution:
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # courses from [0, numCourses-1] given a mapping
-        prereqMap = {}
-        for i in range(numCourses):
-            prereqMap[i] = []
         # map prerequisites to each course, {prereq : [courses]}
+        prereqMap = {i : [] for i in range(numCourses)}
         for course, prereq in prerequisites:
             prereqMap[prereq].append(course)
 
@@ -39,42 +37,3 @@ class Solution:
         prereqMap[course] = [] 
 
         return True
-
-'''
-traversing an adjacency list problem
-
-
-if numCourses < len of the set of classes:
-    return False
-
-[a, b] -> course a requires course b
-
-2 things:
-    cycle detection for matrix?
-    num of courses < len of set of classes
-
-consider a map?
-    - problem 1:
-        0 -> 1
-    -problem 2:
-        0 -> 1
-        1 -> 0
-
-what if we put all prereqs into a map;
-then for each key we follow the path, puttiing the visited nodes
-in a set -- if we collide, then there's a cycle?
-    - a node may map to more than one value; so if a path is valid, 
-        we would still have to backtrack to consider the children
-
-steps:
-    1. put prereqs on a map, where b : a (b the preqreq of a)
-    1a. (if len of map is larger than num of courses, do we return False?)
-        -- alternatively, if we ever "visited more than numCourses nodes", we can return False*
-    1b. courses that don't have prereqs aren't a key in the map
-    2. dfs on each node. if it ever creates a cycle (node already visited), return False
-        -* done here
-    3. if no cycle, backtrack to whereever a branch occurred and dfs that
-    4. if no cycles, return True
-
-O(n) time yeah
-'''
--- neetcode/python/src/graphs/surrounded_regions.py
+++ neetcode/python/src/graphs/surrounded_regions.py
@@ -5,84 +5,51 @@ class Solution:
         pass
 
     def solve(self, board: List[List[str]]) -> None:
-        # any regions that start on a border are unsurroundable
-        # thus if any "O"s are on the border, 
-        # use dfs to get the region and mark them "U"
-        rows, cols = len(board), len(board[0])
-        for row in range(rows):
-            for col in range(cols):
-                if self.isBorderIndex(board, row, col) and board[row][col] == "O":
-                    self.dfs(board, row, col)
+        # run dfs on the border Os, and flip them to Zs
+        visit = set()
+        ROWS, COLS = len(board), len(board[0])
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.isBorderIdx(board, row, col) and board[row][col] == 'O':
+                    self.dfs(board, row, col, visit)
 
-        # re-mark regions
-        for row in range(rows):
-            for col in range(cols):
-                # surroundable regions kept their "O". flip them to "X"
-                if board[row][col] == "O":
-                    board[row][col] = "X"
-                # unsurroundable regions marked with "U". flip them back to "O"
-                if board[row][col] == "U":
-                    board[row][col] = "O"
-
+        # All cells w/ Zs are uncapturable Os
+        # All cells w/ Os are capturable
+        # second pass: flip all Os to Xs, then all Zs to Os
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+                if board[row][col] == 'Z':
+                    board[row][col] = 'O'
+        
         return
 
-    def dfs(
-        self,
-        board: List[List[str]],
-        r: int,
-        c: int
-    ) -> None:
-        # not in range, visited, or not a region: backtrack
-        if not self.validNode(board, r, c):
-            return 
+    # marks all Os adjacent to the source as Z
+    def dfs(self, board: List[List[str]], row: int, col: int, visit: set) -> None:
+        visit.add((row, col))
+        board[row][col] = 'Z'
 
-        board[r][c] = "U"
-        directions = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         for dr, dc in directions:
-            row, col = r + dr, c + dc
-            self.dfs(board, row, col)
-
-        return 
-
-    def validNode(
-        self, 
-        board: List[List[str]], 
-        row: int, 
-        col: int
-    ) -> bool:
-        # in range, not visited, and is a region ("O")
-        return(
-            row >= 0 and row < len(board) and
-            col >= 0 and col < len(board[0]) and
-            board[row][col] == "O"
+            r, c = row + dr, col + dc
+            if self.promising(board, r, c, visit):
+                self.dfs(board, r, c, visit)
+        
+        return
+    
+    def promising(self, board: List[List[str]], row: int, col: int, visit: set) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        return (
+            # indices in range
+            row >= 0 and col >= 0 and
+            row < ROWS and col < COLS and
+            # cell not already visited
+            (row, col) not in visit and
+            # is a cell containing 'O'
+            board[row][col] == 'O'
         )
-
-    def isBorderIndex(self, board: List[List[str]], row: int, col: int) -> bool:
-        return(
-            row == 0 or col == 0 or 
-            row == len(board) - 1 or col == len(board[0]) - 1
-        )
-'''
-my initial guess is to run bfs on the X nodes.
-    - if surrounded by Xs, then capture
-    - if has an adjacent O, check it
-need a "surrounded" condition?
-    - I think we can ignore the border b/c they are not surroundable
-    - so if our bfs ends with an O on an edge, then the region is not surroundable
-
-another idea is to run dfs to collect all the Os in a region. once all the Os hae been exhausted,
-    we backtrack and check the nodes if they are surrounded?
-    - that way if a region ever goes in contact with the border, we just abort and mark the region
-        as not surroundable?
-    - if we do dfs and a neighbor has been visited (but isn't an X), does that mean
-        this node is by default not surroundable? Because if was visited and didn't 
-        change to X, then it touches a node that's at the edge?? so we just mark it 
-        as not surroundable?
-
-
-okay seeing some lessons here. if you are adding a visit set, make sure to add the node 
-before the recursive call
-
-also if you are doing the for loop to change direction indices, use a new local variable
-(in this case, row instead of r)
-'''
+    
+    def isBorderIdx(self, board: List[List[str]], row: int, col: int) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        return row == 0 or col == 0 or row == ROWS - 1 or col == COLS - 1
